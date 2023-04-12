de8fce6490916799c229f2a72df563228cf5af51
Hsinweilin <104602727+Hsinweilin@users.noreply.github.com>
2023-04-11 17:20:10 -0700

    add component
    several .vue file

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+        <Button @btn-click="$emit('toggle-add-task')"  :text="showAddTask ? 'Close' : 'Add Task'" :color="showAddTask ? 'red' : 'green'"/><!--Not just a button tage, it's a actual object from export default-->
+        <Button text="Update Task" color="blue"/>
+        <Button text="Delete Task" color="red"/>
+    </header>
+</template>
+
+<script>
+import Button from './Button'
+
+    export default{
+        name: 'Header',
+        props: {
+            title: String,
+            showAddTask: Boolean
+        },
+        components: {
+            Button
+        },
+        emits: ['toggle-add-task']
+    }
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,22 @@
+<template>
+    <div v-for="task in tasks" v-bind:key="task.id"><!--the first task just one element in a iteration, can be changed to any value like abc, v-bind:key give each iteration a specific key from it's given id, can be used later-->
+        <!--<h3>{{ task.text }}</h3>, access that specific iteration and access the variable text-->
+        <Task @toggle-reminder="$emit('toggle-reminder', task.id)" @delete-task="$emit('delete-task', task.id)" :task="task"/><!--first task, task object of Task component, second task, each task that iterate above-->
+    </div><!--emit can go level by level-->
+</template>
+
+<script>
+import Task from './Task'
+
+export default{
+    name: 'Tasks',
+    props: {
+        tasks: Array,
+    },
+    components: {
+        Task,
+    },
+    emits: ['delete-task', 'toggle-reminder'],
+}
+</script>
+<!--Actively define emits and the emitted event!-->
